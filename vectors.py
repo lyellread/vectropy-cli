f25bc782ced75bc234f72720b71abb4ca5f0438e
lyellread <lyellread@gmail.com>
2018-06-15 21:36:34 -0700

    cmd commit

--- vectors.py
+++ vectors.py
@@ -1,11 +1,30 @@
+#! /usr/bin/env python
+
 ####################################
 #Python Vectors in the Command Line#
 #      Lyell Read | 6/15/2018      #
 ####################################
 
-#  Functionality
+from cmd import Cmd
+
+class VecPrompt (Cmd):
+
+	def do_hello(self,args):
+		"""Says Hello. You give it a name it will greet /you/"""
+		if len(args) == 0:
+			name = ""
+		else:
+			name = args
+			print(args)
+		print("Hello, %s" %name)
+
+	def do_quit(self, args):
+		"""Quits the program... Duh"""
+		print ("Quitting")
+		raise SystemExit
 
 
-
-import sys
-
+if __name__ == "__main__":
+	prompt = VecPrompt()
+	prompt.prompt = 'V&> '
+	prompt.cmdloop('Starting prompt...')
