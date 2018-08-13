1512ada05f28657f6edd80c396604c362c9815c1
lyellread <lyellread@gmail.com>
2018-08-12 23:59:29 -0700

    reduced verbosity and cleaned up. Maths next

--- vectors.py
+++ vectors.py
@@ -12,24 +12,23 @@
 from cmd import Cmd
 import math
 
-version = "1.0.2"
+version = "1.0.3"
 data_file = "data.txt" #replace with path to file if it is somewhere else...
 
 class VecPrompt (Cmd):
 
+
 ## Basic Manipulation Tools ##
+
 
 	def do_input(self,args):
 		"""\n  Input Help Entry\n  ================\n  Func.: Lets you input vector(s) in x- and y- components, mangitude and angle (deg | rad) into the program\n  Usage: + input (xy | deg | rad) (x1 y1 x2 y2 ... | mag1 deg1 mag2 deg2 ... | mag1 rad1 mag2 rad2 ...) \n  Notes: Input checked for even number of args.\n         Enter 0 instead of leaving an entry blank.\n"""
-		print("-----> Started!")
 
 		output=open(data_file, "a+")
 		if len(args) == 0 or not (len(args.split(" "))-1)%2 == 0 or not args.split(" ")[0] in ['xy','deg','rad']:
 			print("-err-> Too few or offset args or improper type")
 			return 0
 		else:
-			print("-----> Passed with arglist: " + args)
-
 			arglist=args.split(" ")
 			type = arglist[0]
 			arglist.remove(arglist[0])
@@ -37,16 +36,16 @@ class VecPrompt (Cmd):
 			for pair in range (0,len(arglist_float)/2):
 				pair_vec = arglist_float [0:2]
 				if type != 'xy':
-					print ("-----> Converting Vector with Magintude " + str(pair_vec[0]) + " and angle " + str(pair_vec[1]) + " " + type)
+					print("-----> Converting both vectors to XY format")
+					#print ("-----> Converting Vector with Magintude " + str(pair_vec[0]) + " and angle " + str(pair_vec[1]) + " " + type)
 					pair_vec= VecPrompt.convert_to_xy(self, pair_vec, type)
-				print ("-----> Adding Vector with X component " + str(pair_vec[0]) + " and Y component " + str(pair_vec[1]))
+				#print ("-----> Adding Vector with X component " + str(pair_vec[0]) + " and Y component " + str(pair_vec[1]))
 
 				#Write to File
 				output.write(str(pair_vec[0]) + " " + str(pair_vec[1])+"\n")
-
 				arglist_float=[arg for arg in arglist_float[2:]]
 
-		print ("-----> Finished!")
+		print ("-----> Input Finished!")
 		output.close()
 
 	def convert_to_xy (self, pair, type):
@@ -64,6 +63,7 @@ class VecPrompt (Cmd):
 
 	def do_clear (self, args):
 		"""\n  Clear Help Entry\n  ================\n  Func.: Clears stored vectors\n  Usage: + clear\n  Notes: Clears all vectors. Permanently ;)\n"""
+
 		VecPrompt.do_print(self,0)
 		#Verify Certainty!
 		verify = raw_input("-ver-> Are you sure you want to clear vectors printed above? (y/n):")
@@ -83,7 +83,8 @@ class VecPrompt (Cmd):
 
 	def do_remove (self, args):
 		# define help menu. ~pretty standard, really~
-		"""\n  Remove Help Entry\n  =================\n  Func.: Removes a vector from storage.\n  Usage: + remove [n1 n2 n3 ...]\n  Notes: If you do not specify line numbers (n1 n2 ...) you can choose interacively\n         The line number input starts at line 0 and specifying the lines as parameters is used internally.\n         If mutiple identical vectors exist and you choose to remove one, it will get all of 'em'\n"""
+		"""\n  Remove Help Entry\n  =================\n  Func.: Removes a vector from storage.\n  Usage: + remove [n1 n2 n3...]\n  Notes: If you do not specify line numbers (n1 n2 ...) you can choose interacively\n         The line number input starts at line 0 and specifying the lines as parameters is used internally.\n         If mutiple identical vectors exist and you choose to remove one, it will get all of 'em'\n"""
+
 		data = open(data_file, "r+")
 		source_data = data.readlines()
 
@@ -91,17 +92,14 @@ class VecPrompt (Cmd):
 		data_validation = []
 		for line in range (0,len(source_data)):
 			data_validation.append(str(line))
-		print("-----> Acceptable input will be checked against " + str(data_validation))
-
 		# if there are no args, then the user is using remove() to remove their choice, thus input needed.
 		if len(args) == 0:
 			VecPrompt.do_print(self,0) #Print out all the vectors in storage
 			#Build an argument list from the input of the user
 			arglist = raw_input("-----> Choose which to remove. Split with spaces: ").split(" ")
 			for arg in arglist:
-				print("-----> Checking arg " + str(arg))
 				if not arg in data_validation:
-					print ("-err-> Validation failed on user entered argument: " + arg + ". Breaking, nothing removed.")
+					print ("-err-> Validation failed on user-entered argument: " + arg + " --> Breaking, nothing removed.")
 					return 0
 					#Break if the arg check failsself.
 			#Otherwise, if the arglist completes, set it to be the definitive one
@@ -112,8 +110,6 @@ class VecPrompt (Cmd):
 
 		#Reverse sort that list of args to take the largest first (thus it will not change the pos of the next item to be removed...)_
 		args.sort(key=int, reverse=True)
-
-		print("-----> Passed with arglist " + str(args))
 
 		remove_list = []
 		data.close()
@@ -128,12 +124,15 @@ class VecPrompt (Cmd):
 					for line in source_data:
 						data.write(line)
 					data.close()
+		print("-----> Remove Finished!")
 
 
 ## Math Tools ##
 
+
 	def do_inverse (self, args):
 		"""\n  Inverse Help Entry\n  ==================\n  Func.: Takes the inverse of a vector. This is the equivalent of rotating the vector by 180 degrees.\n  Usage: + inverse\n  Notes: none\n"""
+
 		if len(args) == 0:
 			VecPrompt.do_print(self,0)
 			inverses = raw_input("-----> Enter all you wish to flip, separated by spaces: ").split(' ')
@@ -148,21 +147,24 @@ class VecPrompt (Cmd):
 			data_validation.append(str(line))
 
 		for arg in inverses:
-			print("-----> Checking argument " + arg)
+
 			if not arg in data_validation:
 				print ("-err-> Input out of range or invalid. Nothing inversed. Breaking!")
 				return 0
 
 		for x in range (0,len(inverses)):
+
 			VecPrompt.do_remove(self,str(inverses[x]))
+
 			temp_vec = source_data[int(inverses[x])]
 			inversed_vec =[]
 			for component in temp_vec.split(" "):
 				inversed_vec.append(-1 * float(component))
 			VecPrompt.do_input(self,"xy " + str(inversed_vec[0]) + " " + str(inversed_vec[1]))
-
+		print ("-----> Inverse Finished")
 
 ## Exiting Tools ##
+
 
 	def do_quit(self, args):
 		"""\n  Quit Help Entry\n  ===============\n  Func.: Quits the program\n  Usage: + quit \n  Notes: No arguments needed\n         Same as `exit`/n"""
@@ -173,6 +175,11 @@ class VecPrompt (Cmd):
 		"""\n  Exit Help Entry\n  ===============\n  Func.: Quits the program\n  Usage: + exit \n  Notes: No arguments needed\n         Same as `quit`\n"""
 		print ("Quitting Immediately")
 		raise SystemExit
+
+
+## Program Start ##
+
+
 if __name__ == "__main__":
 	prompt = VecPrompt()
 	prompt.prompt = '+ '
